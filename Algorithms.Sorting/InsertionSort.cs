368021e443067df1491d8c83254c12f1d922d489
annajoyw <arealjoyw@gmail.com>
2021-05-29 17:06:49 -0400

    sorting

--- Algorithms.Sorting/InsertionSort.cs
+++ Algorithms.Sorting/InsertionSort.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Sorting
 {
-    public class Class1
+    class InsertionSort
     {
     }
 }
---
+++ DS.LinkedLists/TraversingLinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DS.LinkedLists.SinglyLinkedList;
+
+namespace DS.LinkedLists
+{
+    class TraversingLinkedList
+    {
+        private Node start;
+
+        public TraversingLinkedList()
+        {
+            start = null;
+        }
+
+        public void CountNodes()
+        {
+            int n = 0;
+            Node p = start;
+            while(p != null)
+            {
+                n++;
+                p = p.next;
+            }
+        }
+            
+    }
+}
