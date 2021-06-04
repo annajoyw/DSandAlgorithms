71ba894421f991ab15327017de6fa7ab9737868f
annajoyw <arealjoyw@gmail.com>
2021-06-04 17:06:32 -0400

    longest prefix array problem

--- CodePlayGround/Class2.cs
+++ CodePlayGround/Class2.cs
@@ -97,11 +97,26 @@ namespace CodePlayGround
         }
     }
 
-    public class Solution
+    public class Solution98765
     {
-        public bool IsValid(string s)
+        public string LongestCommonPrefix(string[] strs)
         {
-            
+            if (strs.Length == 0 || Array.IndexOf(strs, "") != -1)
+                return "";
+            string res = strs[0];
+            int i = res.Length;
+            foreach (string word in strs)
+            {
+                int j = 0;
+                foreach (char c in word)
+                {
+                    if (j >= i || res[j] != c)
+                        break;
+                    j += 1;
+                }
+                i = Math.Min(i, j);
+            }
+            return res.Substring(0, i);
         }
     }
 
---
+++ DS.Arrays/LongestCommonPrefix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Arrays
+{
+    class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixx(string[] strs)
+        {
+            if (strs.Length == 0 || Array.IndexOf(strs, "") != -1)
+                return "";
+
+
+            string res = strs[0];
+            int i = res.Length;
+            foreach (string word in strs)
+            {
+                int j = 0;
+                foreach (char c in word)
+                {
+                    if (j >= i || res[j] != c)
+                        break;
+                    j += 1;
+                }
+                i = Math.Min(i, j);
+            }
+
+            return res.Substring(0, i);
+        }
+    }
+}
